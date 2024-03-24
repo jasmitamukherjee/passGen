90dcfc47d130e59cdb276d6d4d097cf19afc2578
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-24 19:26:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import * as Yup from 'yup'
@@ -11,6 +12,58 @@ const PasswordSchema =Yup.object().shape({
   .required('Length is required')
 })
 export default function App() {
+  const [password, setPassword] = useState('')
+  const [isPassGenerated, setIsPassGenerated] = useState(false)
+  const [lowerCase, setLowerCase] = useState(true)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [symbols, setSymbols] = useState(false)
+
+  const generatePasswordString = (passwordLength) => {
+    let characterList = '';
+
+    const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+    const digitChars = '0123456789'
+    const specialChars = '!@#$%^&*()_+'
+    if(upperCase){
+      characterList+=upperCase
+    }
+    if(lowerCase){
+      characterList+=lowerCase
+    }
+    if(numbers){
+      characterList+=numbers
+    }
+    if(symbols){
+      characterList+=symbols
+    }
+
+    const passwordResult = createPassword(characterList,passwordLength)
+    setPassword(passwordResult)
+    setIsPassGenerated(true)
+  }
+
+  const createPassword = (characters,passwordLength)=>{
+    let result = ''
+    for( let i =0;i<passwordLength;i++){
+      const characterIndex = Math.round(Math.random() * characters.length)
+      result+=characters.charAt(characterIndex)
+    }
+    return result
+
+  }
+  const resetPasswordState = ()=>{
+
+    setPassword('')
+    setIsPassGenerated(false)
+    setLowerCase(false)
+    setUpperCase(false)
+    setNumbers(false)
+    setSymbols(false)
+
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
