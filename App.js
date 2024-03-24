eb33ec5a077e70ed36eafcf7240d6c75998aa169
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-24 18:56:50 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Yup from 'yup'
+
+
+const PasswordSchema =Yup.object().shape({
+  passwordLength : Yup.number()
+  .min(4,'Should be minimum of 4 characters')
+  .max(16,'Should be maximum of 16 characters')
+  .required('Length is required')
+})
 export default function App() {
   return (
     <View style={styles.container}>
